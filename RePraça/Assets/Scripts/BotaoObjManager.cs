1afbc93ab779811a12d54f4b842710ff6d66001e
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2022-10-26 17:00:24 -0300

    botoes objetos automatizados

--- RePraça/Assets/Scripts/BotaoObjManager.cs
+++ RePraça/Assets/Scripts/BotaoObjManager.cs
@@ -12,9 +12,10 @@ public class BotaoObjManager : MonoBehaviour
 
     public GameObject currentImgDestaque;
     public GameObject newImgDestaque;
-
     public ObjetosData dados;
-    //public ObjetosData newDados;
+
+    public ObjetosData[] listaTodosDados;
+    public GameObject prefabBotaoAdd;
 
     private GameObject objetoToBeConstruido;
     private BuildingManager buildingManager;
@@ -23,6 +24,18 @@ public class BotaoObjManager : MonoBehaviour
     {
         //coloca o objeto building manager da scene na variavel do codigo
         buildingManager = GameObject.Find("BuildingManager").GetComponent<BuildingManager>();
+
+        for (int i = 0; i < listaTodosDados.Length; i++)
+        {
+            var botaoNovo = Instantiate(prefabBotaoAdd);
+            botaoNovo.transform.SetParent(this.gameObject.transform, false);
+            
+            BotaoObjSelect botaoCriado = botaoNovo.GetComponent<BotaoObjSelect>();
+            //botaoCriado.imagemObjeto.sprite = botaoCriado.dadosObj.imagemObjeto;
+
+            botaoCriado.dadosObj = listaTodosDados[i];
+            botaoCriado.Start();
+        }
     }
 
     public void BotaoObjClicado()
