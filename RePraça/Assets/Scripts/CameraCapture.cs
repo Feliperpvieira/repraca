ae5f232efdcbeea9591fe9caeed885ed09027f65
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2022-11-02 22:17:37 -0300

    versao inicial da tela de captura

--- RePraça/Assets/Scripts/CameraCapture.cs
+++ RePraça/Assets/Scripts/CameraCapture.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static NativeGallery;
 
 public class CameraCapture : MonoBehaviour
 {
-    public RenderTexture rt;
+    public RenderTexture rtVistaTopo;
+    public RenderTexture rtVistaAngulo;
 
     string album = "rePraca";
     MediaSaveCallback callback = null;
 
-    public static string ScreenShotName(int width, int height) //define o nome do arquivo
+    public static string ScreenShotName(string nomeCena, string angulo) //define o nome do arquivo
     {
-        return string.Format("praca_{0}x{1}_{2}.png",
-                               width, height,
+        return string.Format("praça_{0}-{1}_{2}.png",
+                               nomeCena, angulo,
                                System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")); //data e hora atual
 
         //return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
@@ -25,17 +27,23 @@ public class CameraCapture : MonoBehaviour
 
     public void SaveTexture()
     {
-        byte[] bytes = toTexture2D(rt).EncodeToPNG(); //transforma a renderTexture em texture 2d
-        string fileName = ScreenShotName(1080, 1080); //define o nome do arquivo
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        byte[] imagemTopo = toTexture2D(rtVistaTopo, 1200, 1200).EncodeToPNG(); //transforma a renderTexture em texture 2d
+        string fileName = ScreenShotName(sceneName, "topo"); //define o nome do arquivo
         //System.IO.File.WriteAllBytes(fileName, bytes);
-        NativeGallery.SaveImageToGallery(bytes, album, fileName, callback); //plugin native gallery https://github.com/yasirkula/UnityNativeGallery
+        NativeGallery.SaveImageToGallery(imagemTopo, album, fileName, callback); //plugin native gallery https://github.com/yasirkula/UnityNativeGallery
 
         //Debug.Log(string.Format("Took screenshot to: {0}", fileName));
+
+        byte[] imagemAngulo = toTexture2D(rtVistaAngulo, 1920, 1200).EncodeToPNG(); //transforma a renderTexture em texture 2d
+        string fileNameAng = ScreenShotName(sceneName, "angulo"); //define o nome do arquivo
+        NativeGallery.SaveImageToGallery(imagemAngulo, album, fileNameAng, callback);
     }
 
-    Texture2D toTexture2D(RenderTexture rTex)
+    Texture2D toTexture2D(RenderTexture rTex, int width, int height)
     {
-        Texture2D tex = new Texture2D(1080, 1080, TextureFormat.RGB24, false);
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
         RenderTexture.active = rTex;
         tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
         tex.Apply();
