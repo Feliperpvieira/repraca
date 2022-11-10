412befe956733631e75727302f855ecd85e92758
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2022-11-10 13:13:20 -0300

    Add files via upload

--- galeria/js/app.js
+++ galeria/js/app.js
@@ -1,58 +1,44 @@
 const gallery = document.getElementById("gallery");
 const popup = document.getElementById("popup");
 const selectedImage = document.getElementById("selectedImage");
-const imageIndexes = [1, 2, 3];
 const imagesTotal = 99;
-const selectedIndex = null;
 
-/* imageIndexes.forEach(i => {
-    const image = document.createElement("img");
-    image.src = "images/cover_episode-" + i + ".jpg";
-    image.alt = "Praça número " + i;
-    image.classList.add("galleryImg");
-
-    //console.log(i);
-    image.addEventListener("click", () => {
-        //popup
-        popup.style.transform = "translateY(0)";
-        selectedImage.src = "images/cover_episode-" + i + ".jpg";
-        selectedImage.alt = "Praça número " + i;
-    })
-
-    gallery.appendChild(image);
-}) */
-
+//cria a grid de imagens
 for (let i = imagesTotal; i > 0; i--) { 
-    const imgUrl = "images/cover_episode-" + i + ".jpg"
-    checkIfImageExists(imgUrl, (exists) => {
-        if (exists) {
-        const image = document.createElement("img");
-        image.src = "images/cover_episode-" + i + ".jpg";
-        image.alt = "Praça número " + i;
-        image.classList.add("galleryImg");
+    const imgUrl = "images/cover_episode-" + i + ".jpg" //define o url da imagem
 
-        //console.log(i);
-        image.addEventListener("click", () => {
-            //popup
-            popup.style.transform = "translateY(0)";
-            selectedImage.src = "images/cover_episode-" + i + ".jpg";
-            selectedImage.alt = "Praça número " + i;
-        })
+    checkIfImageExists(imgUrl, (exists) => { //checa se o url existe
 
-        gallery.appendChild(image);
+        if (exists) { //se existir cria a imagem
+            const image = document.createElement("img");
+            image.src = imgUrl;
+            image.alt = "Praça número " + i;
+            image.classList.add("galleryImg");
+
+            //console.log(i);
+            image.addEventListener("click", () => {
+                //popup
+                popup.style.transform = "translateY(0)";
+                selectedImage.src = imgUrl;
+                selectedImage.alt = "Praça número " + i;
+            })
+
+            gallery.appendChild(image);
         } else {
-        console.log("falhou" + i)
+            console.log("falhou" + i)
         }
     })
 
 }
 
-popup.addEventListener("click", () => {
+popup.addEventListener("click", () => { //fecha o popup quando clica nele
     popup.style.transform = "translateY(-100%)";
     popup.src = "";
     popup.alt = "";
 })
 
+//funcao que confere se um url de imagem funciona - https://codepen.io/kallil-belmonte/pen/KKKRoyx
+//só funciona na versão online, localmente dá erro
 function checkIfImageExists(url, callback) {
     const img = new Image();
     img.src = url;
